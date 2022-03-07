482fe8e6d963a25ed0da19238d36f3931003cf1f
Javkhlan <r.javkhlan@gmail.com>
2022-03-07 17:34:51 +0800

    first commit

---
+++ pages/about/[slug].js
@@ -0,0 +1,4 @@
+function haha() {
+  return <div>end delgerengui neg yumnii tuhai baigaa</div>;
+}
+export default haha;
---
+++ pages/about/index.js
@@ -0,0 +1,5 @@
+import styles from "./about.module.css";
+function about() {
+  return <div className={styles.dorjoo}>this page is for about ulalala</div>;
+}
+export default about;
